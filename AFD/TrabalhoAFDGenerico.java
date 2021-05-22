82b805d154bfcde6535e2df64550eb26b30b9a98
fonsecaPedr0 <pedropbf5.pp@gmail.com>
2021-05-21 23:43:33 -0300

    Alteracao implementacao generica

--- AFD/TrabalhoAFDGenerico.java
+++ AFD/TrabalhoAFDGenerico.java
@@ -1,51 +1,62 @@
-//package trabalhoAFD;
+package trabalhoAFD;
 
-public class TrabalhoAFDGenerico{
+import java.util.Scanner;
+
+public class trabalhoAFDGenerico {
+	//alterar valores de acordo com a maquina AFD		
+	static int[] aceitacao = {1}; 
+	static int [][] transicao = {{0,1}, {1,0}};
+	static int estadoInicial = 0;
 	
-	static int[] aceitacao = {1};
-	static int[][] transicao = {{0,1}, {1,0}};
-	static int estadoInicial = 0;  
-
-	public static void main(String args[]) {
+		public static void main(String[] args) 
+		{	
+			Scanner scanner = new Scanner(System.in);
 			
-		String entrada = args[0];
-		int estado = estadoInicial;
-		int posicao = 0;
+			String cadeia;
+			int posicao = 0, estado = estadoInicial;
+			char simbolo;
+			boolean aceita = false;
 			
-			while(posicao < entrada.length()) {
-				impressaoRegrasP(entrada, estado, posicao);
-
-				char elemento = entrada.charAt(posicao);
-
-				int elementoInt = Integer.parseInt(elemento+"");
+			System.out.print("Informe a cadeia a ser validade: ");
+			cadeia = scanner.next();
+			
+				while(posicao < cadeia.length())
+				{
+					impressaoRegrasP(cadeia, estado, posicao);
 					
-				estado = transicao[estado] [elemento];
-				
-				posicao++;
+					simbolo = cadeia.charAt(posicao);
+					int simboloInt = Integer.parseInt(simbolo+"");
+						
+					estado = transicao[estado][simboloInt];
+					
+					posicao++;
+				}
+			
+			
+			impressaoRegrasP(cadeia, estado, posicao);
+			
+			for(int i:aceitacao)
+			{
+				if(estado==i) aceita = true;
 			}
 			
-		impressaoRegrasP(entrada, estado, posicao);
-		
-		boolean aceita = false;
-
-			for(int i:aceitacao){
-				if(estado == 1) aceita = true;
+			if(aceita)
+			{
+				System.out.println("A cadeia e valida!!");
 			}
-
-		if(aceita){
-			System.out.print("Aceita a cadeia!");
-		}else{
-			System.out.print("Não aceita a cadeia!");
+			else 
+			{
+				System.out.println("A cadeia nao e valida!");
+			}
+			
+			
+			scanner.close();
 		}
 			
-	}
-	
-	
-	public static void impressaoRegrasP(String entrada, int estado, int posicao) 
-	{
-		System.out.print(entrada.substring(0, posicao));
-		System.out.print("[Q"+estado+"]");
-		System.out.println(entrada.substring(posicao));
-		
-	}
+		public static void impressaoRegrasP(String entrada, int estado, int posicao)
+		{
+			System.out.print(entrada.substring(0, posicao));
+			System.out.print("[Q"+estado+"]");
+			System.out.println(entrada.substring(posicao));
+		}
 }
